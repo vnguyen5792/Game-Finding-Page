0f6a00918d71ee305b2aa36cbffb4699a82c08c6
vnguyen5792 <vietaddisonguyen@gmail.com>
2024-01-16 11:00:27 -0600

    implemented a simple event handler for an on mouse click action

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,6 +1,10 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+
+  //event handler with type annotation
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
@@ -9,8 +13,10 @@ function ListGroup() {
       {/* this lets you use condition statements to have different elements */}
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
