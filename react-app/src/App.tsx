518f88b03cfde653bb2c819fa0ad111abfc8a8fa
vnguyen5792 <vietaddisonguyen@gmail.com>
2024-01-17 22:10:32 -0600

    passing children to a component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,20 +1,25 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  //this is a simple function that will print the item to the console
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // //this is a simple function that will print the item to the console
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
 
   return (
     <div>
-      <ListGroup
+      {/* <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
-      />
+      /> */}
+
+      <Alert>
+        hello <span>world</span>
+      </Alert>
     </div>
   );
 }
